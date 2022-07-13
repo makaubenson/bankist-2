1a575b988173babc572778d01a29bf3155309ecb
Benson <bensonmakau2000@gmail.com>
2022-07-13 11:23:17 +0300

    numeric separators
     underscore(_)

--- script.js
+++ script.js
@@ -348,3 +348,6 @@ labelBalance.addEventListener('click', function () {
 //Numeric Separators
 const diameter = 287_460_000_000;
 console.log(diameter);
+
+const priceCents = 345_99;
+console.log(priceCents); //34599
