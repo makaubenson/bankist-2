80be894b68df585cfde7ffe1ae9290e5d9846a52
Benson <bensonmakau2000@gmail.com>
2022-07-14 08:56:19 +0300

    internationalizing movements on bankist app

--- script.js
+++ script.js
@@ -106,13 +106,18 @@ const displayMovements = function (acc, sort = false) {
 
     const date = new Date(acc.movementsDates[i]);
     const displayDate = formatMovementDate(date, acc.locale);
+
+    const formattedMov = new Intl.NumberFormat(acc.locale, {
+      style: 'currency',
+      currency: acc.currency,
+    }).format(mov);
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
         <div class="movements__date">${displayDate}</div>
-        
+        <div class="movements__value">${formattedMov}</div>
       </div>
     `;
 
